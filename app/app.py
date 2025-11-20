a701c09cff3ca70ae44b81bd24a89c985d68765c
Matheus Maximo de Araujo <L00196726@atu.ie>
2025-11-20 13:41:34 +0000

    Initial App (#1)

---
+++ app/app.py
@@ -0,0 +1,30 @@
+import uuid
+from datetime import datetime
+
+from flask import Flask, jsonify, redirect
+
+app = Flask(__name__)
+
+# API method to generate a short URL
+@app.route("/shorten", methods=["POST"])
+def shorten():
+    return jsonify({
+        "code": str(uuid.uuid4()),
+        "short_url": "short_url",
+        "long_url": "long_url",
+        "created_at": datetime.now(datetime.timezone.utc).isoformat() + "Z"
+    }), 201
+
+# API method to get a shortened URL
+@app.route("/<code>", methods=["GET"])
+def redirect_code(code):
+    return redirect("https://www.atu.ie", code=302)
+
+# Health check method for the Kubernetes to determine if the POD is healthy
+@app.route("/health")
+def health():
+    return jsonify({"status": "ok"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
