df8babccc5df2521dfaf40610ce24cfaea705bc8
Matheus Maximo de Araujo <L00196726@atu.ie>
2025-11-26 21:17:33 +0000

    Initial App

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 
 from flask import Flask, jsonify, redirect
 
@@ -12,12 +12,12 @@ def shorten():
         "code": str(uuid.uuid4()),
         "short_url": "short_url",
         "long_url": "long_url",
-        "created_at": datetime.now(datetime.timezone.utc).isoformat() + "Z"
+        "created_at": datetime.now(timezone.utc).isoformat() + "Z"
     }), 201
 
 # API method to get a shortened URL
 @app.route("/<code>", methods=["GET"])
-def redirect_code(code):
+def get(code):
     return redirect("https://www.atu.ie", code=302)
 
 # Health check method for the Kubernetes to determine if the POD is healthy
