d3b1ec7660564229815410992a331ff61ef04e4c
Matheus Maximo de Araujo <L00196726@atu.ie>
2025-12-11 09:28:55 +0000

    Add destroy script for Terraform infrastructure

---
+++ scripts/destroy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+TF_DIR="infra/terraform"
+HEALTH_TIMEOUT=60 # Wait 60secs for health OK
+
+echo "Starting deploy"
+
+cd $TF_DIR
+
+echo "Terraform init..."
+terraform init -upgrade -input=false
+
+echo "Terraform validate..."
+terraform validate
+
+echo "Terraform plan..."
+terraform plan -out=tfplan -input=false
+
+echo "Terraform destroy..."
+terraform destroy -auto-approve tfplan
